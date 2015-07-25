d58be385114191a6134ccb01f0a2d49bccd57f10
fernandokm <ferkmatsumoto@hotmail.com>
2015-07-25 01:05:01 -0300

    Updated From and SafeFrom methods

--- number.go
+++ number.go
@@ -1,6 +1,8 @@
-// +build false
-
 package numeric
+
+import (
+	"math/big"
+)
 
 type Number struct {
 	storage Numeric
@@ -10,42 +12,44 @@ func SafeFrom(value interface{}) (number Number, err error) {
 	var n Numeric
 	switch v := value.(type) {
 	case int8:
-		n = Integer(v)
+		n = Float(v)
 	case int16:
-		n = Integer(v)
+		n = Float(v)
 	case int32:
-		n = Integer(v)
+		n = Float(v)
 	case int64:
-		n = Integer(v)
+		n = Float(v)
 	case int:
-		n = Integer(v)
-
+		n = Float(v)
 	case uint8:
-		n = UInteger(v)
+		n = Float(v)
 	case uint16:
-		n = UInteger(v)
+		n = Float(v)
 	case uint32:
-		n = UInteger(v)
+		n = Float(v)
 	case uint64:
-		n = UInteger(v)
+		n = Float(v)
 	case uint:
-		n = UInteger(v)
-
+		n = Float(v)
 	case float32:
 		n = Float(v)
 	case float64:
 		n = Float(v)
+
+	case big.Rat:
+		x := BigFloat(v)
+		n = &x
+	case big.Int:
+		n = (*BigFloat)(new(big.Rat).SetInt(&v))
+
 	case Numeric:
 		n = v
 	case Number:
 		return v, nil
 	default:
-		err = ConversionError{value}
+		return Number{}, ConversionError{value}
 	}
-	if n != nil {
-		number = Number{n}
-	}
-	return
+	return Number{n}, nil
 }
 
 func From(value interface{}) Number {
