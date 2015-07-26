18f2490117130b2fabcba3f4f23aca7c01b2fd2c
fernandokm <ferkmatsumoto@hotmail.com>
2015-07-26 16:55:40 -0300

    Added documentation for the Numeric interface and types

--- numeric.go
+++ numeric.go
@@ -4,6 +4,30 @@ import (
 	"math/big"
 )
 
+//TODO: change Zero() and One methods to New(int32)
+
+// Numeric is an interface that represents any numeric type,
+// such as float64. Default implementations exist for the types
+// float64 (type Float) and big.Rat(type BigFloat).
+//
+// Methods Zero() and One() should return the values 0 and 1.
+// The Negate(), Add(), Subtract(), Multiply() and Divide() methods
+// are aliases for the corresponding operations.
+// To compare two Numeric values, the method CompareTo() should be called.
+// For inputs x and y, it should return:
+//		x.CompareTo(y)
+//      // 0 if x == y
+//			// 1 if x > y
+//			// -1 if x < y
+// The available conversion methods are Float64() and BigRat().
+// Numeric also supports string conversion through the Stringer interface.
+//
+// Promote() and ShouldPromote() are part of a value promotion system.
+// If a value is to big or small to be adequately represented in its
+// Numeric type, it can be promoted (converted) to a type that has a
+// larger capacity. If this happens, the method ShouldPromote() should
+// return true and the method Promote() should return the promoted
+// value.
 type Numeric interface {
 	Zero() Numeric
 	One() Numeric
@@ -20,5 +44,11 @@ type Numeric interface {
 	String() string
 }
 
+// Float is an implementation of the Numeric interface for the float64 type.
+// It can be promoted to a BigFloat
+// and will return true on ShouldPromote() if it's equal to +/-Infinity.
 type Float float64
+
+// BigFloat is an implementation of the Numeric interface for the big.Rat type.
+// It cannot be promoted.
 type BigFloat big.Rat
